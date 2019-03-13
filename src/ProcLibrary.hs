730fbad9066863b5b34eccd27add42ab262b657d
Martin Snyder <martin@martinsnyder.net>
2019-03-12 20:20:01 -0400

    Complete implenentation of 'lookOutBelow'

--- src/ProcLibrary.hs
+++ src/ProcLibrary.hs
@@ -1,6 +1,6 @@
 module ProcLibrary (roomProcedureLibrary) where
 
-import Data.Map
+import Data.Map (Map, fromList)
 
 import GameObj
 import Mob
@@ -11,6 +11,11 @@ import Message
 import Target
 import CommandHelpers
 
+marbleRailingId = (0,1)
+westCourtyardId = (1,5)
+eastCourtyardId = (1,6)
+eastGalleryId = (1,8)
+
 roomProcedureLibrary :: Map String RoomProcedure
 roomProcedureLibrary = fromList([
         ("unstableFloor", (\ command args world -> case name command of
@@ -19,19 +24,26 @@ roomProcedureLibrary = fromList([
         ))
       , ("lookOutBelow", (\ command args world -> case (name command, target1 args) of
             ("jump", TargetItem item) ->
-                if (itemId item == (0,1)) then do
+                if (itemId item == marbleRailingId) then do
                     room <- getRoom (locationId $ actor args) world
-                    destinationRoomId <- return (1,5)
-                    destinationRoom <- getRoom destinationRoomId world
-                    destinationRoomMobs <- getRoomMobs destinationRoom world
-                    updateWorld world [ updateRoom (removeMobId $ Mob.id $ actor args) $ locationId $ actor args
-                                      , updateRoom (addMobId $ Mob.id $ actor args) destinationRoomId
-                                      , sendMessageTo args MsgActorRoom [Desc Actor, Sur " " $ Verb Actor "fly" "flies", Const "over ", Desc Target1, Const " to the courtyard below."]
-                                      , sendMessageTo args (MsgRoom destinationRoomId) [Desc Actor, Sur " " $ Verb Actor "crash" "crashes", Const $ "down from above!"]
-                                      , sendMessageTo args (MsgRoom destinationRoomId) [Const "The floor buckles violently when ", Desc Actor, Sp, Verb Actor "fall" "falls", Const " on it."]
-                                      , updateMob (\mob -> Right $ mob { locationId = destinationRoomId }) $ Mob.id $ actor args
-                                      , sendTextMob (actor args) $ lookRoomShort destinationRoom destinationRoomMobs
-                                      ]
+                    westCourtyardRoom <- getRoom westCourtyardId world
+                    westCourtyardMobs <- getRoomMobs westCourtyardRoom world
+                    eastCourtyardRoom <- getRoom eastCourtyardId world
+                    mobsIdsGettingThrown  <- Right $ mobIds eastCourtyardRoom
+                    eastGalleryRoom <- getRoom eastGalleryId world
+                    world' <- updateWorld world [ updateRoom (removeMobId $ Mob.id $ actor args) $ locationId $ actor args
+                                                , updateRoom (addMobId $ Mob.id $ actor args) westCourtyardId
+                                                , sendMessageTo args MsgActorRoom [Desc Actor, Sur " " $ Verb Actor "fly" "flies", Const "over ", Desc Target1, Const " to the courtyard below."]
+                                                , sendMessageTo args (MsgRoom westCourtyardId) [Desc Actor, Sur " " $ Verb Actor "crash" "crashes", Const $ "down from above!"]
+                                                , sendMessageTo args (MsgRoom westCourtyardId) [Const "The floor buckles violently when ", Desc Actor, Sp, Verb Actor "fall" "falls", Const " on it."]
+                                                , updateMob (\mob -> Right $ mob { locationId = westCourtyardId }) $ Mob.id $ actor args
+                                                , sendTextMob (actor args) $ lookRoomShort westCourtyardRoom westCourtyardMobs
+                                                , sendMessageTo args (MsgRoom eastCourtyardId) [Const "The floor buckles violently and throws everyone to the gallery above!"]
+                                                , sendMessageTo args (MsgRoom eastGalleryId) [Const "There is a crash below and bodies fly into the gallery"]
+                                                , updateRoom (\room -> Right $ room { mobIds = (mobIds eastCourtyardRoom) ++ (mobIds eastGalleryRoom) }) eastGalleryId
+                                                , updateRoom (\room -> Right $ room { mobIds = [] }) eastCourtyardId
+                                                ]
+                    foldl (\ acc mobId -> acc >>= (\accWorld -> updateMob (\mob -> Right $ mob { locationId = eastGalleryId }) mobId accWorld)) (Right world') mobsIdsGettingThrown
                 else
                     Right world
             _ -> Right world
